b94f562b209f801c3cbfa35a11e0ff873df56c2d
Rushhav <rushav182@gmail.com>
2026-01-13 17:38:02 +0530

    vpc module

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,19 @@
 # output "default_vpc_info" {
 #   value = data.aws_vpc.default
 # }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id's" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_id's" {
+  value = aws_subnet.private[*].id
+}
+
+output "db_subnet_id's" {
+  value = aws_subnet.db[*].id
+}
